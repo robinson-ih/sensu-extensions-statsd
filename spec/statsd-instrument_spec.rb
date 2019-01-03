4a149dc0edfc93ba17fc85995a7f37bc87e186cf
Rajiv <rajiv.g@introhive.com>
2019-01-03 18:11:18 +0530

    Updated test cases

--- spec/statsd-instrument_spec.rb
+++ spec/statsd-instrument_spec.rb
@@ -1,13 +1,13 @@
 require File.join(File.dirname(__FILE__), 'helpers')
 require 'sensu/extensions/statsd'
 require 'statsd-instrument'
-# Using datadog protocol
-StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("127.0.0.1:8125", :datadog)
 
-describe 'Sensu::Extension::StatsD' do
+describe 'Statsd extension with datadog protocol' do
   include Helpers
 
   before do
+    # Using datadog protocol
+    StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("127.0.0.1:8125", :datadog)
     @extension = Sensu::Extension::StatsD.new
     @extension.settings = {
       client: {
@@ -163,6 +163,97 @@ describe 'Sensu::Extension::StatsD' do
         timer(2) do
           @extension.safe_run do |output, status|
             expect(output).to match(/foo\.statsd\.counters\.test1\.count 11 \d+ t1:1,t2:2/)
+            expect(status).to eq(0)
+            async_done
+          end
+        end
+      end
+    end
+  end
+
+end
+
+describe 'Statsd extension with statsd protocol' do
+  include Helpers
+
+  before do
+    # Using statsd protocol
+    StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("127.0.0.1:8125", :statsd)
+    @extension = Sensu::Extension::StatsD.new
+    @extension.settings = {
+      client: {
+        name: 'foo'
+      },
+      statsd: {
+        flush_interval: 1
+      }
+    }
+    @extension.logger = Sensu::Logger.get(log_level: :fatal)
+  end
+
+  it 'can run' do
+    async_wrapper do
+      @extension.safe_run do |output, status|
+        expect(output).to eq('')
+        expect(status).to eq(0)
+        async_done
+      end
+    end
+  end
+
+  it 'can create graphite plaintext metrics' do
+    async_wrapper do
+      timer(1) do
+        StatsD.increment('test1.count', 10)
+        StatsD.gauge('test1.value', 20)
+        StatsD.measure('test1.time', 30)
+        timer(2) do
+          @extension.safe_run do |output, status|
+            expect(output).to match(/foo\.statsd\.gauges\.test1\.value 20\.0/)
+            expect(output).to match(/foo\.statsd\.counters\.test1\.count 10/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.lower 30\.0/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.mean 30\.0/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.upper 30\.0/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.upper_90 30\.0/)
+            expect(status).to eq(0)
+            async_done
+          end
+        end
+      end
+    end
+  end
+
+  it 'does not support relative gauges' do
+    async_wrapper do
+      timer(1) do
+        StatsD.gauge('tcp', +2)
+        StatsD.gauge('tcp', -2)
+        StatsD.gauge('tcp', -3)
+        timer(2) do
+          @extension.safe_run do |output, status|
+            expect(output).to match(/foo\.statsd\.gauges\.tcp -3\.0/)
+            expect(status).to eq(0)
+            async_done
+          end
+        end
+      end
+    end
+  end
+
+  it 'ignore tags' do
+    async_wrapper do
+      timer(1) do
+        StatsD.increment('test1.count', 10, tags: { t1: 10, t2: 'value1' } )
+        StatsD.gauge('test1.value', 20, tags: { t3: 10, t4: 'value2' } )
+        StatsD.measure('test1.time', 30, tags: { t5: 10, t6: 'value3' } )
+        timer(2) do
+          @extension.safe_run do |output, status|
+            expect(output).to match(/foo\.statsd\.gauges\.test1\.value 20\.0 \d+\n/)
+            expect(output).to match(/foo\.statsd\.counters\.test1\.count 10 \d+\n/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.lower 30\.0 \d+\n/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.mean 30\.0 \d+\n/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.upper 30\.0 \d+\n/)
+            expect(output).to match(/foo\.statsd\.timers\.test1\.time\.upper_90 30\.0 \d+\n/)
             expect(status).to eq(0)
             async_done
           end
